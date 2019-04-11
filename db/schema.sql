5ddb1a10db33a8dfa61cba3c8e264144fa76bcce
Derek <44953753+dbiafore@users.noreply.github.com>
2019-04-10 22:46:47 -0700

    Update schema.sql and seeds.sql to add users

--- db/schema.sql
+++ db/schema.sql
@@ -4,47 +4,43 @@ CREATE DATABASE tiny_donations_db;
 
 USE tiny_donations_db;
 
-CREATE TABLE donors(
+CREATE TABLE users (
 	id INT NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	password_hash VARCHAR(255) NOT NULL,
-	FOREIGN KEY (donations_id) REFERENCES donations(id),
-	PRIMARY KEY (id)
-);
-
-
-CREATE TABLE donation_centers(
-	id INT NOT NULL AUTO_INCREMENT,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	password_hash VARCHAR(255) NOT NULL,
+	role INT NOT NULL,
 	name VARCHAR(255) NOT NULL,
-	country VARCHAR(255) NOT NULL,
 	street VARCHAR(255) NOT NULL,
 	city VARCHAR(255) NOT NULL,
+	state  VARCHAR(255) NOT NULL,
 	zip INT NOT NULL,
+	country VARCHAR(255) NOT NULL,
+	hours VARCHAR(225) NOT NULL,
+	days  VARCHAR(225) NOT NULL,
 	directions VARCHAR(255) NOT NULL,
-	acceptable_donations_id,
-	donations_id,
-	FOREIGN KEY (acceptable_donations_id) REFERENCES acceptable_donations(id),
-	FOREIGN KEY (donations_id) REFERENCES donations(id),
 	PRIMARY KEY (id)
 );
+
+/* FOREIGN KEY (acceptable_donations_id) REFERENCES acceptable_donations(id),
+FOREIGN KEY (donations_id) REFERENCES  donations(id) */
 
 
 CREATE TABLE acceptable_donations(
 	id INT NOT NULL AUTO_INCREMENT,
-	type VARCHAR(255) NOT NULL UNIQUE,
-	quanity INT NOT NULL,
-	PRIMARY KEY (id)	
+	type VARCHAR(255) NOT NULL,
+	quantity INT NOT NULL,
+	user_id INT NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE donations(
+	id INT NOT NULL AUTO_INCREMENT,
 	date DATETIME NOT NULL,
 	type VARCHAR(255) NOT NULL,
 	quantity INT NOT NULL,
-	donors_id,
-	donation_centers_id,
-	FOREIGN KEY (donation_centers_id) REFERENCES donation_centers(id),
-	FOREIGN KEY (donors_id) REFERENCES donors(id),
+	user_id INT NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	PRIMARY KEY (id)
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,14 @@
 /*
 	Make a seeds.sql file where you insert 5 actors into the actors table
 */
-USE actors_db;
+USE tiny_donations_db;
 
-INSERT INTO actors (actor_name) VALUES	("slyvester stalone");
-INSERT INTO actors (actor_name) VALUES	("ryan renold");
-INSERT INTO actors (actor_name) VALUES	("arnold shortsanager");
-INSERT INTO actors (actor_name) VALUES	("vin diesel");
-INSERT INTO actors (actor_name) VALUES	("selma high-eek");
+INSERT INTO users (email, password_hash, role, name, street, city, state, zip, country, hours, days, directions) VALUES	
+("joesmith@email.com", "erofjieasfeawh", 1, "joe smith", "32 First Street", "San Francisco", "CA", "94034", "USA", "4-5pm", "m-f", "Place items in the green bin"),
+("jeanie@email.com", "erosaasffdase8flewh", 1, "jeanie jones", "9387 Red Street", "Gilroy", "CA", "95454", "USA", "3-5pm", "s,s", "Black box on south wall"),
+("bobsanders@email.com", "333roerwew8flewh", 1, "bob sanders", "39478 OAK road", "Willow Glen", "CA", "94034", "USA", "8-9am", "t, th", "Put on table under window"),
+("ravi@email.com", "342rfawofjie8flewh", 1, "ravi bin", "1 astor place", "San Francisco", "CA", "94034", "USA", "4-5pm", "m-f", "Leave with reception"),
+("susanlee@email.com", "agferfgaerflerfaersdfg", 1, "sue lee", "3987 Ashland Drive", "Oakland", "CA", "94034", "USA", "4-5pm", "sunday", "The cardboard box marked 'donations'"),
+("jakejohnson@email.com", "erofjeafae", 1, "j johnson", "76 Whiskey hill LN ", "San Antonio", "TX", "97874", "USA", "9-5pm", "m, w, f", "Leave at door");
+
+
