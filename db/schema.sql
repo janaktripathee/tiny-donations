e8a56eea9b04538040f04f82ea0cc384aa53b8c5
Derek <44953753+dbiafore@users.noreply.github.com>
2019-04-13 21:43:39 -0700

    Updating schema.sql users table

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,19 @@ CREATE DATABASE tiny_donations_db;
 USE tiny_donations_db;
 
 CREATE TABLE users (
-    id INT AUTO_INCREMENT, 
-    name VARCHAR(255),
-    PRIMARY KEY(id)
+	id INT NOT NULL AUTO_INCREMENT,
+	-- email VARCHAR(255) NOT NULL UNIQUE,
+	-- password_hash VARCHAR(255) NOT NULL,
+	-- role INT NOT NULL,
+	name VARCHAR(255),
+	street VARCHAR(255),
+	city VARCHAR(255),
+	state  VARCHAR(255),
+	zip INT,
+	hours VARCHAR(225),
+	days  VARCHAR(225),
+	instructions VARCHAR(255),
+	lat DECIMAL(10, 8),
+	lng DECIMAL(10, 8),
+	PRIMARY KEY (id)
 );
-
